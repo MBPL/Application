127101336d1ccf75f6adfe38c3fdc5d580620c23
Matteo <matteo.mougeot@gmail.com>
2016-04-26 15:59:07 +0200

    merge

--- app/src/main/java/mbpl/graphical/passwords/TabFragment3.java
+++ app/src/main/java/mbpl/graphical/passwords/TabFragment3.java
@@ -16,6 +16,9 @@ public class TabFragment3 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+
         return inflater.inflate(R.layout.menu_tab_fragment_3, container, false);
     }
 }
